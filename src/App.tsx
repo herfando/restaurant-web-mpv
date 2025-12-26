42063ef0684febc4a58fd39d61f486cdad3133d2
Herfando <herfando_91@yahoo.com>
2025-12-26 13:57:28 +0700

    edit env home and services

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './index.css';
 // import Login from '@/pages/01_Login';
 // import Register from './pages/02_Register';
 // import AppRoutes from '@/routes/Routes';
-// import NavbarGuest from './components/layout/01_NavbarGuest';
-import NavbarGuest2 from './components/layout/01_NavbarGuest2';
-// import Home from '@/pages/03_Home';
+import NavbarGuest from './components/layout/01_NavbarGuest';
+// import NavbarGuest2 from './components/layout/01_NavbarGuest2';
+import Home from '@/pages/03_Home';
 // import Detail from './pages/04_Detail';
 // import Category from './pages/05_Category';
 // import MyCart from './pages/06_MyCart';
@@ -12,7 +12,7 @@ import NavbarGuest2 from './components/layout/01_NavbarGuest2';
 // import Success from './pages/08_Success';
 // import MyOrders from './pages/09_MyOrders';
 // import Review from './pages/10_Review';
-import Profile from './pages/11_Profile';
+// import Profile from './pages/11_Profile';
 import Footer from './components/layout/03_Footer';
 
 function App() {
@@ -21,9 +21,9 @@ function App() {
       {/* <AppRoutes /> */}
       {/* <Login /> */}
       {/* <Register /> */}
-      {/* <NavbarGuest /> */}
-      <NavbarGuest2 />
-      {/* <Home /> */}
+      <NavbarGuest />
+      {/* <NavbarGuest2 /> */}
+      <Home />
       {/* <Detail /> */}
       {/* <Category /> */}
       {/* <MyCart /> */}
@@ -31,7 +31,7 @@ function App() {
       {/* <Success /> */}
       {/* <MyOrders /> */}
       {/* <Review /> */}
-      <Profile />
+      {/* <Profile /> */}
       <Footer />
     </>
   );
